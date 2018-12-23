e5354ef9e855f18a7c5ba8af983d1b84cdcfcef0
xuhuaguang <huaguang131400>
2018-12-23 16:59:38 +0800

    first commit

---
+++ src/main/java/com/rxlm/springboottools/SpringbootToolsApplication.java
@@ -0,0 +1,14 @@
+package com.rxlm.springboottools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootToolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootToolsApplication.class, args);
+    }
+
+}
+
